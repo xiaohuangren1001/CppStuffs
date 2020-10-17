c9ffc718801f8365d23965fe4d9a4a752e367303
Jack <68948146+xiaohuangren1001@users.noreply.github.com>
2020-10-17 11:31:35 +0800

    用C++写的屑3 快捷方式
    实际是隐藏代码用的（

---
+++ shortcut.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define max(a, b) (a) > (b) ? (a) : (b)
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define abs(a) (a) > 0 ? (a) : -(a)
+#define max3(a, b, c) max(max(a, b), c)
+#define max4(a, b, c, d) max(max3(a, b, c), d)
+#define max5(a, b, c, d, e) max(max4(a, b, c, d), e)
+#define max6(a, b, c, d, e, f) max(max5(a, b, c, d, e), f)
+#define max7(a, b, c, d, e, f, g) max(max6(a, b, c, d, e, f), g)
+#define max8(a, b, c, d, e, f, g, h) max(max7(a, b, c, d, e, f, g), h)
+#define max9(a, b, c, d, e, f, g, h, i) max(max8(a, b, c, d, e, f, g, h), i)
+#define max10(a, b, c, d, e, f, g, h, i, j) max(max9(a, b, c, d, e, f, g, h, i), j)
+
+bool is_prime(int a)
+{
+	for (int i = 2; i * i < a; i++)
+	{
+		if (i % a == 0)
+			return false;
+		else continue;
+	}
+	return true;
+}
+template <typename T>
+T input()
+{
+	T a;
+	cin >> a;
+	return a;
+}
+template <typename T>
+void print(T t)
+{
+	cout << t;
+}
+string reverse(string s)
+{
+	int l = s.length();
+	for (int i = 0; i < l / 2 + 1; i++)
+	{
+		char t = s[i];
+		s[i] = s[l-i];
+		s[l-i] = t;
+	}
+	return s;
+}
